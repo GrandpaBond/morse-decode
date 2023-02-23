7a9ee4236e6c960796be837939f94528a681954c
GrandpaBond <89019204+GrandpaBond@users.noreply.github.com>
2023-02-23 18:31:30 +0000

    (using button A to test)

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+
+DOT_MIN = 200
+DASH_MIN = 500
+LETTER_PAUSE = 1000
+LOUD = 200
+QUIET = 100
+MORSE_TABLE = [ 3,	63,	123,  79,  47,  99,  95, 119, 127, 114,
+              135,	59,  87,  71, 103,  75, 129,  64,   1,  88,
+                0, 104,  81,  50, 136,  52, 140, 146, 108,   2,
+                3,	24,  20,   0,  16,   0,   0,   0,  12,   0,
+                0,	 0,   0,   0,   0,   0,   8,  28,   0,   0,
+                0,	 0,   0,   0,   0,  32,   0,   0,	0,  36,
+                0,	40,   4,   0,  48,  49,  50,  51,  52,  53,
+               54,	55,  56,  57,  65,  66,  67,  68,  69,  70,
+               71,	72,  73,  74,  75,  76,  77,  78,  79,  80,
+               81,	82,  83,  84,  85,  86,  87,  88,  89,  90]
+morse_step = 0
+command = ''
+busy = False
+beeping = False
+beepOn = 0
+beepOff = 0
+
 def obey(action):
     basic.show_string(action)
 
@@ -8,55 +31,68 @@ def decode(this):
         return 'O'
     else:
         return ' '
-      
+
+def morse_dot():
+    global morse_step
+    v = MORSE_TABLE[-morse_step]
+    if (v & 1) > 0:
+        morse_step = (morse_step * 2) - 2
+    else:
+        morse_step = 0
+    
+def morse_dash():
+    global morse_step
+    v = MORSE_TABLE[-morse_step]
+    if (v & 2) > 0:
+        morse_step = (morse_step * 2) - 1
+    else:
+        morse_step = 0
+        
+def morse_end():
+    global morse_step
+    v = MORSE_TABLE[-morse_step] // 4
+    if v > 0:
+        morse_step = MORSE_TABLE[v+63] 
+    else:
+        morse_step = 0
+
+    pass
+                    
 def morse_check():
-    global DASH, LETTER, LOUD, QUIET  
-    global listening, beeping
-    letter = ' '
-    pattern = ""
-    if listening:
-        if beeping: 
+    global busy, beeping, command
+    if busy:
+        if beeping:
             # if input.sound_level() < QUIET:
             if not input.button_is_pressed(Button.A):
                 beepOff = control.millis()
-                beeping = False 
+                beeping = False
                 # analyse length of beep that just stopped
                 length = beepOff - beepOn
-                if length > DASH:
-                    pattern += '-'
-                else:
-                    pattern += '.'                
-            # else just let the beep continue...
+                if length > DASH_MIN:
+                    morse_dash()
+                elif length > DOT_MIN:
+                    morse_dot()
+            # ...else wait for the beep to stop
         else: # how long since last beep stopped?
             gap = control.millis() - beepOff
-            if gap > LETTER:
-                listening = False
-                letter = decode(pattern)
-                pattern = ''
-            # else keep listening for more dots or dashes
+            if gap > LETTER_PAUSE:
+                morse_end()
+                busy = False
+            # ...else keep listening for more dots or dashes
     else:
         # if input.sound_level() > LOUD:
         if input.button_is_pressed(Button.A):
             beepOn = control.millis()
-            listening = True
-    return (letter)
+            busy = True
 
 def on_forever():
     # do all kinds of stuff...
     basic.show_icon(IconNames.HEART)
     # check for morse command
-    command = morse_check()   
-    if command != ' ':
-        obey(command)
+    morse_check()
+    if morse_step > 0:
+        obey(String.from_char_code(morse_step))
 
     
 
-DASH = 500
-LETTER = 1000
-LOUD = 200
-QUIET = 100
-listening = False
-beeping = False
-beepOn = 0
-beepOff = 0
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
+let DOT_MIN = 200
+let DASH_MIN = 500
+let LETTER_PAUSE = 1000
+let LOUD = 200
+let QUIET = 100
+let MORSE_TABLE = [3, 63, 123, 79, 47, 99, 95, 119, 127, 114, 135, 59, 87, 71, 103, 75, 129, 64, 1, 88, 0, 104, 81, 50, 136, 52, 140, 146, 108, 2, 3, 24, 20, 0, 16, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 8, 28, 0, 0, 0, 0, 0, 0, 0, 32, 0, 0, 0, 36, 0, 40, 4, 0, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
+let morse_step = 0
+let command = ""
+let busy = false
+let beeping = false
+let beepOn = 0
+let beepOff = 0
 function obey(action: string) {
     basic.showString(action)
 }
 
-function decode(this_: string): string {
+function decode(this_: any): string {
     if (this_ == "---") {
         return "S"
     } else if (this_ == "...") {
@@ -13,16 +25,47 @@ function decode(this_: string): string {
     
 }
 
-function morse_check(): string {
+function morse_dot() {
+    
+    let v = MORSE_TABLE[-morse_step]
+    if ((v & 1) > 0) {
+        morse_step = morse_step * 2 - 2
+    } else {
+        morse_step = 0
+    }
+    
+}
+
+function morse_dash() {
+    
+    let v = MORSE_TABLE[-morse_step]
+    if ((v & 2) > 0) {
+        morse_step = morse_step * 2 - 1
+    } else {
+        morse_step = 0
+    }
+    
+}
+
+function morse_end() {
+    
+    let v = Math.idiv(MORSE_TABLE[-morse_step], 4)
+    if (v > 0) {
+        morse_step = MORSE_TABLE[v + 63]
+    } else {
+        morse_step = 0
+    }
+    
+    
+}
+
+function morse_check() {
     let beepOff: number;
     let length: number;
     let gap: number;
     let beepOn: number;
     
-    
-    let letter = " "
-    let pattern = ""
-    if (listening) {
+    if (busy) {
         if (beeping) {
             //  if input.sound_level() < QUIET:
             if (!input.buttonIsPressed(Button.A)) {
@@ -30,49 +73,39 @@ function morse_check(): string {
                 beeping = false
                 //  analyse length of beep that just stopped
                 length = beepOff - beepOn
-                if (length > DASH) {
-                    pattern += "-"
-                } else {
-                    pattern += "."
+                if (length > DASH_MIN) {
+                    morse_dash()
+                } else if (length > DOT_MIN) {
+                    morse_dot()
                 }
                 
             }
             
         } else {
-            //  else just let the beep continue...
+            //  ...else wait for the beep to stop
             //  how long since last beep stopped?
             gap = control.millis() - beepOff
-            if (gap > LETTER) {
-                listening = false
-                letter = decode(pattern)
-                pattern = ""
+            if (gap > LETTER_PAUSE) {
+                morse_end()
+                busy = false
             }
             
         }
         
     } else if (input.buttonIsPressed(Button.A)) {
         beepOn = control.millis()
-        listening = true
+        busy = true
     }
     
-    return letter
 }
 
-let DASH = 500
-let LETTER = 1000
-let LOUD = 200
-let QUIET = 100
-let listening = false
-let beeping = false
-let beepOn = 0
-let beepOff = 0
 basic.forever(function on_forever() {
     //  do all kinds of stuff...
     basic.showIcon(IconNames.Heart)
     //  check for morse command
-    let command = morse_check()
-    if (command != " ") {
-        obey(command)
+    morse_check()
+    if (morse_step > 0) {
+        obey(String.fromCharCode(morse_step))
     }
     
 })
